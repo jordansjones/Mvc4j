3b165fef6b932fb1c482a811a1993c6117ac477d
Jordan S. Jones <jordansjones@gmail.com>
2012-05-10 17:55:13 -0600

    Ported ParserBase

---
+++ Razor/src/main/java/nextmethod/web/razor/parser/ParserBase.java
@@ -0,0 +1,54 @@
+package nextmethod.web.razor.parser;
+
+import nextmethod.Tuple;
+import nextmethod.web.razor.parser.syntaxtree.SpanBuilder;
+import nextmethod.web.razor.text.SourceLocation;
+
+import javax.annotation.Nonnull;
+
+import static nextmethod.web.razor.resources.Mvc4jRazorResources.RazorResources;
+
+public abstract class ParserBase {
+
+	private ParserContext context;
+
+	public ParserContext getContext() {
+		return context;
+	}
+
+	public void setContext(@Nonnull final ParserContext context) {
+		assert this.context == null : "Context has already been set for this parser!";
+		this.context = context;
+		this.context.assertOnOwnerTask();
+	}
+
+	public boolean isMarkerParser() {
+		return false;
+	}
+
+	protected abstract ParserBase getOtherParser();
+
+	public abstract void buildSpan(@Nonnull final SpanBuilder span, @Nonnull final SourceLocation start, @Nonnull final String content);
+
+	public abstract void parseBlock();
+
+	/**
+	 * Markup Parsers need the parseDocument method since
+	 * the markup parser is the first parser to hit the document and the
+	 * logic may be different than the parseBlock method.
+	 */
+	public void parseDocument() {
+		assert isMarkerParser();
+		throw new UnsupportedOperationException(RazorResources().getString("parserIsNotAMarkupParser"));
+	}
+
+	/**
+	 * Markup Parsers need the parseSection method since
+	 * the markup parser is the first parser to hit the document and the
+	 * logic may be different than the parseBlock method.
+	 */
+	public void parseSection(@Nonnull final Tuple<String, String> nestingSequence, final boolean caseSensitive) {
+		assert isMarkerParser();
+		throw new UnsupportedOperationException(RazorResources().getString("parserIsNotAMarkupParser"));
+	}
+}
---
+++ Razor/src/main/java/nextmethod/web/razor/parser/ParserContext.java
@@ -0,0 +1,10 @@
+package nextmethod.web.razor.parser;
+
+// TODO
+public class ParserContext {
+
+	void assertOnOwnerTask() {
+		// TODO
+	}
+
+}
