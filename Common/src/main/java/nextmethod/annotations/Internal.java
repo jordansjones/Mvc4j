c2533446b1c5a1b0eedd97a6a60faad03f47f161
Jordan S. Jones <jordansjones@gmail.com>
2012-10-28 20:42:13 -0600

    Javadoc

--- Common/src/main/java/nextmethod/annotations/Internal.java
+++ Common/src/main/java/nextmethod/annotations/Internal.java
@@ -5,7 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD})
+/**
+ * Signifies that a public API (public class, method, or field) is not intended
+ * for general consumption and is subject to incompatible changes, or even
+ * removal, in a future release. This is due to a lack of more granular access
+ * modifiers in the Java language.
+ */
 @Retention(RetentionPolicy.CLASS)
+@Target({
+	ElementType.ANNOTATION_TYPE,
+	ElementType.CONSTRUCTOR,
+	ElementType.FIELD,
+	ElementType.METHOD,
+	ElementType.TYPE
+})
 public @interface Internal {
 }
